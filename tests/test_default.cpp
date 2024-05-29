e1c4181770df279c6e08a734442e0fc07b87f2d2
Molin. L <molin@live.cn>
2024-05-29 23:40:36 +0800

    feat(Build): Use conan to manage dependency.

---
+++ tests/test_default.cpp
@@ -0,0 +1,18 @@
+/* 
+ * --------------------------------------------------------------
+ * File:          test_default.cpp
+ * Project:       Code
+ * Created:       Wednesday, 29th May 2024 5:47:57 pm
+ * @Author:       Molin Liu
+ * Contact:       molinliu@live.com
+ * Modified:      Wednesday, 29th May 2024 5:47:59 pm
+ * Copyright      © Celestial 2023 - 2024
+ * --------------------------------------------------------------
+ */
+#include <gtest/gtest.h>
+#include <iostream>
+
+TEST(TestDefault, Default)
+{
+    EXPECT_TRUE(true);
+}
